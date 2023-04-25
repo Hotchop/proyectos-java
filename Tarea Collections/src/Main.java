4c473597358e7e119cbd68c717fc97e1a2cabe5f
Hotchop <mirandanic1996@gmail.com>
2023-04-25 01:12:03 -0300

    Tarea de Colecciones

--- Tarea Collections/src/Main.java
+++ Tarea Collections/src/Main.java
@@ -1,5 +1,43 @@
+import web.hotchop.models.Vuelo;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.ListIterator;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+        ArrayList<Vuelo> listado = new ArrayList<>();
+        listado.add(new Vuelo("AAL 933","New York","Santiago",LocalDateTime.parse("29/08/2021 05:39",formatter),62));
+        listado.add(new Vuelo("LAT 755","Sao Paulo","Santiago",LocalDateTime.parse("31/08/2021 04:45",formatter),47));
+        listado.add(new Vuelo("SKU 621","Rio De Janeiro","Santigo",LocalDateTime.parse("30/08/2021 16:00",formatter),52));
+        listado.add(new Vuelo("DAL 147","Atlanta","Santigo",LocalDateTime.parse("29/08/2021 13:22",formatter),59));
+        listado.add(new Vuelo("AVA 241","Bogota","Santigo",LocalDateTime.parse("31/08/2021 14:05",formatter),25));
+        listado.add(new Vuelo("AMX 10","Mexico City","Santigo",LocalDateTime.parse("31/08/2021 05:20",formatter),29));
+        listado.add(new Vuelo("IBE 6833","Londres","Santigo",LocalDateTime.parse("30/08/2021 08:45",formatter),55));
+        listado.add(new Vuelo("LAT 2479","Frankfurt","Santigo",LocalDateTime.parse("29/08/2021 07:41",formatter),51));
+        listado.add(new Vuelo("SKU 803","Lima","Santigo",LocalDateTime.parse("30/08/2021 10:35",formatter),48));
+        listado.add(new Vuelo("LAT 533","Los Ángeles","Santigo",LocalDateTime.parse("29/08/2021 09:14",formatter),59));
+        listado.add(new Vuelo("LAT 1447","Guayaquil","Santigo",LocalDateTime.parse("31/08/2021 08:33",formatter),31));
+        listado.add(new Vuelo("CMP 111","Panama City","Santigo",LocalDateTime.parse("31/08/2021 15:15",formatter),29));
+        listado.add(new Vuelo("LAT 705","Madrid","Santigo",LocalDateTime.parse("30/08/2021 08:14",formatter),47));
+        listado.add(new Vuelo("AAL 957","Miami","Santigo",LocalDateTime.parse("29/08/2021 22:53",formatter),60));
+        listado.add(new Vuelo("ARG 5091","Buenos Aires","Santigo",LocalDateTime.parse("31/08/2021 09:57",formatter),32));
+        listado.add(new Vuelo("LAT 1283","Cancún","Santigo",LocalDateTime.parse("31/08/2021 04:00",formatter),35));
+        listado.add(new Vuelo("LAT 579","Barcelona","Santigo",LocalDateTime.parse("29/08/2021 07:45",formatter),61));
+        listado.add(new Vuelo("AAL 945","Dallas-Fort Worth","Santigo",LocalDateTime.parse("30/08/2021 07:12",formatter),58));
+        listado.add(new Vuelo("LAT 501","París","Santigo",LocalDateTime.parse("29/08/2021 18:29",formatter),49));
+        listado.add(new Vuelo("LAT 405","Montevideo","Santigo",LocalDateTime.parse("30/08/2021 15:45",formatter),39));
+
+        Collections.sort(listado);
+        System.out.println(Collections.max(listado));
+        ListIterator<Vuelo> vueloListIterator = listado.listIterator();
+        int minPasajeros = 999;
+        while (vueloListIterator.hasNext()){
+            minPasajeros = Math.min(vueloListIterator.next().getCantidadPasajeros(),minPasajeros);
+        }
+        System.out.println("minPasajeros = " + minPasajeros);
     }
 }
--- Tarea Collections/src/web/hotchop/models/Vuelo.java
+++ Tarea Collections/src/web/hotchop/models/Vuelo.java
@@ -1,22 +1,78 @@
 package web.hotchop.models;
 
-import java.util.Date;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
-public class Vuelo {
+public class Vuelo implements Comparable<Vuelo>{
     private String nombre;
     private String origen;
     private String destino;
-    private Date fechaLlegada;
-    private Date horaLlegada;
+    private LocalDateTime fechaLlegada;
     private int cantidadPasajeros;
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
 
-    public Vuelo(String nombre, String origen, String destino, Date fechaLlegada, Date horaLlegada, int cantidadPasajeros) {
+    public Vuelo(String nombre, String origen, String destino, LocalDateTime fechaLlegada, int cantidadPasajeros) {
         this.nombre = nombre;
         this.origen = origen;
         this.destino = destino;
         this.fechaLlegada = fechaLlegada;
-        this.horaLlegada = horaLlegada;
         this.cantidadPasajeros = cantidadPasajeros;
     }
 
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getOrigen() {
+        return origen;
+    }
+
+    public void setOrigen(String origen) {
+        this.origen = origen;
+    }
+
+    public String getDestino() {
+        return destino;
+    }
+
+    public void setDestino(String destino) {
+        this.destino = destino;
+    }
+
+    public LocalDateTime getFechaLlegada() {
+        return fechaLlegada;
+    }
+
+    public void setFechaLlegada(LocalDateTime fechaLlegada) {
+        this.fechaLlegada = fechaLlegada;
+    }
+
+    public int getCantidadPasajeros() {
+        return cantidadPasajeros;
+    }
+
+    public void setCantidadPasajeros(int cantidadPasajeros) {
+        this.cantidadPasajeros = cantidadPasajeros;
+    }
+
+    @Override
+    public int compareTo(Vuelo o) {
+        return this.fechaLlegada.compareTo(o.fechaLlegada);
+    }
+
+
+    @Override
+    public String toString() {
+        return "Vuelo{" +
+                "nombre='" + nombre + '\'' +
+                ", origen='" + origen + '\'' +
+                ", destino='" + destino + '\'' +
+                ", fechaLlegada=" + fechaLlegada.format(formatter) +
+                ", cantidadPasajeros=" + cantidadPasajeros +
+                '}';
+    }
 }
